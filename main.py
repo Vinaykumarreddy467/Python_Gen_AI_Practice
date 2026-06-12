633457bab3f8c449ac0f43bc390bb57ccab36db4
Vinay Kumar Reddy <vinaykumarreddy467.com>
2026-06-12 11:09:43 +0530

    ignore

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+  
+load_dotenv()
+
+
+huggingface=os.getenv("HUGGINGFACE_API_KEY")
+
+def main():
+    print("Hello, World!")
+    print(huggingface[:20])
+    # print(HUGGINGFACE_API_KEY[:7])
+
+if __name__ == "__main__": main()
