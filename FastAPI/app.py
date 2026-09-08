f532b71d29bfcfaeb89701e320bcdb78a0a5f2f5
Vinay Kumar Reddy <vinaykumarreddy467.com>
2026-09-08 13:25:50 +0530

    auto: update app.py

--- FastAPI/app.py
+++ FastAPI/app.py
@@ -8,6 +8,10 @@ async def root():
     return {"message": "Hello World"}
 
 items={"foo": "The Foo Wrestlers", "bar": "The Bar Fighters", "baz": "The Baz Brawlers", "qux": "The Qux Quarrelers", "quux": "The Quux Questers", "corge": "The Corge Champions", "grault": "The Grault Gladiators", "garply": "The Garply Guardians", "waldo": "The Waldo Warriors", "fred": "The Fred Fighters", "plugh": "The Plugh Protectors", "xyzzy": "The Xyzzy Xenophobes", "thud": "The Thud Titans"}
+@app.get("/items")
+async def read_items():
+    return items
+
 @app.get("/items/{item_id}")
 async def read_item(item_id: int, q: str = None):
     if item_id in items:
